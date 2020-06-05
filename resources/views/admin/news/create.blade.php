13e9e502fa1341e10297cf172d67f3b4dcdee0a2
”1146-yamagishi” <rikurikurikuriku0515@gmail.com>
2020-06-05 01:16:27 +0000

    test

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -10,23 +10,24 @@
     <body>
         {{-- layouts/admin.blade.phpを読み込む --}}
         
-@extends('layouts.admin')
-
-
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'ニュースの新規作成')
+        @extends('layouts.admin') 
 
 
 
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
-@section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 mx-auto">
-                <h2>ニュース新規作成</h2>
+        {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+        @section('title', 'ニュースの新規作成')
+
+
+
+        {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+        @section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h2>ニュース新規作成</h2>
+                </div>
             </div>
         </div>
-    </div>
-@endsection
+        @endsection
     </body>
 </html>
